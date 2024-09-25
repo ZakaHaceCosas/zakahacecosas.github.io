a5070c94f91a0e7c2c3e8114e1456948f549d821
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-09-25 18:22:48 +0200

    TypeExperiment: Make labels.ts typesafe

--- js/labels.ts
+++ js/labels.ts
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const boxes = document.querySelectorAll("box");
 
-    boxes.forEach((box) => {
-        const allLabels = box.getAttribute("labels");
+    boxes.forEach((box: Element) => {
+        const allLabels: string | null = box.getAttribute("labels");
         if (!allLabels) {
-            return // no labels = just ignore
+            return; // no labels = just ignore
         }
-        const labels = allLabels.split(",");
-        const flexElement = box.querySelector("flex");
+
+        const labels: string[] = allLabels.split(",");
+        const flexElement: Element | null = box.querySelector("flex");
+        if (!flexElement) {
+            console.error("If this is running, it means a card WITH labels doesn't have the <flex> element. Check your code.")
+        }
 
         // creates a <labeler> and adds it to the <box>
-        const labeler = document.createElement("labeler");
+        const labeler: HTMLElement = document.createElement("labeler");
         box.insertBefore(labeler, flexElement);
 
-        labels.forEach((label) => {
-            const labelElement = document.createElement("label");
+        labels.forEach((label: string) => {
+            const labelElement: HTMLElement = document.createElement("label");
             labelElement.innerText = label;
 
             // labels are added to the labeler to avoid issues with flex styling
