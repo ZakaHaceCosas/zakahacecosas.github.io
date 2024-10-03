df58fdc32086daee6566095e7c9640ec4f894e8b
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-10-03 16:48:55 +0200

    Refactor labels.ts

--- js/labels.js
+++ js/labels.js
@@ -6,19 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!allLabels) {
             return; // no labels = just ignore
         }
-        const labels = allLabels.split(",");
         const flexElement = box.querySelector("flex");
         if (!flexElement) {
             console.error("If this is running, it means a card WITH labels doesn't have the <flex> element. Check your code.");
+            throw new Error("Error: No flex element on labeled card.");
         }
-        // creates a <labeler> and adds it to the <box>
+        // creates a <labeler> and adds labels to it before inserting it into the <box>
         const labeler = document.createElement("labeler");
-        box.insertBefore(labeler, flexElement);
-        labels.forEach((label) => {
+        const fragment = document.createDocumentFragment();
+        allLabels.split(",").forEach((label) => {
             const labelElement = document.createElement("label");
             labelElement.innerText = label;
-            // labels are added to the labeler to avoid issues with flex styling
-            labeler.appendChild(labelElement);
+            fragment.appendChild(labelElement);
         });
+        // labels are added to the labeler to avoid issues with flex styling
+        // now this is done in a single operation (for performance)
+        labeler.appendChild(fragment);
+        // append the labeler before the flex element
+        box.insertBefore(labeler, flexElement);
     });
 });
--- ts/labels.ts
+++ ts/labels.ts
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const boxes: NodeListOf<Element> = document.querySelectorAll<Element>("box");
+    const boxes: NodeListOf<Element> = document.querySelectorAll("box");
 
     boxes.forEach((box: Element) => {
         const allLabels: string | null = box.getAttribute("labels");
@@ -7,22 +7,27 @@ document.addEventListener("DOMContentLoaded", () => {
             return; // no labels = just ignore
         }
 
-        const labels: string[] = allLabels.split(",");
         const flexElement: Element | null = box.querySelector("flex");
         if (!flexElement) {
-            console.error("If this is running, it means a card WITH labels doesn't have the <flex> element. Check your code.")
+            console.error("If this is running, it means a card WITH labels doesn't have the <flex> element. Check your code.");
+            throw new Error("Error: No flex element on labeled card.");
         }
 
-        // creates a <labeler> and adds it to the <box>
+        // creates a <labeler> and adds labels to it before inserting it into the <box>
         const labeler: HTMLElement = document.createElement("labeler");
-        box.insertBefore(labeler, flexElement);
+        const fragment = document.createDocumentFragment();
 
-        labels.forEach((label: string) => {
+        allLabels.split(",").forEach((label: string) => {
             const labelElement: HTMLElement = document.createElement("label");
             labelElement.innerText = label;
-
-            // labels are added to the labeler to avoid issues with flex styling
-            labeler.appendChild(labelElement);
+            fragment.appendChild(labelElement);
         });
+
+        // labels are added to the labeler to avoid issues with flex styling
+        // now this is done in a single operation (for performance)
+        labeler.appendChild(fragment);
+
+        // append the labeler before the flex element
+        box.insertBefore(labeler, flexElement);
     });
 });
