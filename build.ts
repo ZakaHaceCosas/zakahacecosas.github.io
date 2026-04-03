3159c58297fc519803e1543bf2a2f851ee524c0b
ZakaHaceCosas <zakahacecosas@protonmail.com>
2026-04-04 01:03:31 +0200

    i don't actually need to be moving this

--- build.ts
+++ build.ts
@@ -1,12 +1,7 @@
 import * as fs from "fs";
 
 await Bun.$`bun run astro build`;
-const dir = fs.readdirSync("docs/_astro/");
-dir.forEach((v) => fs.renameSync("docs/_astro/" + v, "docs/" + v))
-fs.rmdirSync("docs/_astro");
 const cnt = fs.readFileSync("docs/index.html", { encoding: "utf-8" });
-const newStr = cnt
-    .replace("<head>", `<head><link rel="stylesheet" href="/${dir.find((v) => v.endsWith(".css"))}">`)
-    .replaceAll(/(?=<!--)([\s\S]*?)-->/g, "");
+const newStr = cnt.replaceAll(/(?=<!--)([\s\S]*?)-->/g, "");
 fs.writeFileSync("docs/index.html", newStr);
 export { };
