6105d0af64c91c01e9a662d7faad0d5b112eb7fa
ZakaHaceCosas <zakahacecosas@protonmail.com>
2026-04-04 00:50:13 +0200

    fix builds

--- build.ts
+++ build.ts
@@ -2,11 +2,11 @@ import * as fs from "fs";
 
 await Bun.$`bun run astro build`;
 const dir = fs.readdirSync("docs/_astro/");
-fs.renameSync("docs/_astro/" + dir[0], "docs/" + dir[0]);
+dir.forEach((v) => fs.renameSync("docs/_astro/" + v, "docs/" + v))
 fs.rmdirSync("docs/_astro");
 const cnt = fs.readFileSync("docs/index.html", { encoding: "utf-8" });
 const newStr = cnt
-    .replace("<head>", `<head><link rel="stylesheet" href="/${dir[0]}">`)
+    .replace("<head>", `<head><link rel="stylesheet" href="/${dir.find((v) => v.endsWith(".css"))}">`)
     .replaceAll(/(?=<!--)([\s\S]*?)-->/g, "");
 fs.writeFileSync("docs/index.html", newStr);
-export {};
+export { };
